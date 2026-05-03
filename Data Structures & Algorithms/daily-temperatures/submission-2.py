4b9b4360d037e704f443562033bcdb7d722489a3
Andrew Moy <105390880+acymoy@users.noreply.github.com>
2026-05-03 14:40:23 -0400

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.py
@@ -0,0 +1,19 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        results = [0] * len(temperatures)
+
+        for i in range(len(temperatures), -1, -1):
+            j = i + 1
+            while j < len(temperatures):
+                print('j', j)
+                # if the current j value is greater
+                if temperatures[i] < temperatures[j]:
+                    results[i] = j - i
+                    break
+                # if there is not greater value in list
+                if not results[j]:
+                    break
+                # otherwise skip forward by j days
+                else:
+                    j += results[j]
+        return results
